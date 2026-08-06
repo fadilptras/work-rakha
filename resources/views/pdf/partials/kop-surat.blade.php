2f7935ccecb49cf1da28ebbd0e4fcb7ccdbc9d2b
M Fadhillah Putra Sinaga <fadhilsinaga3@gmail.com>
2026-08-06 14:59:08 +0700

    updt warna teks kop surat

--- resources/views/pdf/partials/kop-surat.blade.php
+++ resources/views/pdf/partials/kop-surat.blade.php
@@ -4,7 +4,7 @@
             <img src="{{ public_path('images/logo-kop.png') }}" style="width:85px;">
         </td>
         <td style="vertical-align:middle; text-align:center; padding-right:30px;">
-            <div style="font-size:18px; font-weight:bold; color: #4075b8; font-family: 'Times New Roman', Times, serif; letter-spacing: 0.5px;">PT RAKHA NUSANTARA MEDIKA</div>
+            <div style="font-size:18px; font-weight:bold; color: #244d85; font-family: 'Times New Roman', Times, serif; letter-spacing: 0.5px;">PT RAKHA NUSANTARA MEDIKA</div>
             <div style="font-size:12px; font-family: 'Times New Roman', Times, serif; margin-top: 6px;">
                 Perumahan Taman Kenari Blok C2 NO. 12B Kel. Cimahpar, Kec. Bogor Utara,
             </div>
