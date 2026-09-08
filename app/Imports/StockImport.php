0f608d16c286a9206ebab4782e9d2c35a44217ec
M Fadhillah Putra Sinaga <fadhilsinaga3@gmail.com>
2026-09-09 05:31:30 +0700

    updt import stock code

--- app/Imports/StockImport.php
+++ app/Imports/StockImport.php
@@ -2,7 +2,6 @@
 
 namespace App\Imports;
 
-use App\Models\Barang;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithCalculatedFormulas;
